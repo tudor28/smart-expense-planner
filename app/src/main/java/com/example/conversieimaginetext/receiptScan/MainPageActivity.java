437a93d80e30f210117d12993f015365ad64cfc4
Tudor Nitescu <tudor.alin97@yahoo.com>
2021-05-19 16:21:10 +0300

    Added recognition for total and shop

--- app/src/main/java/com/example/conversieimaginetext/receiptScan/MainPageActivity.java
+++ app/src/main/java/com/example/conversieimaginetext/receiptScan/MainPageActivity.java
@@ -9,6 +9,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -41,7 +42,10 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static android.Manifest.permission.CAMERA;
 
@@ -49,32 +53,34 @@ public class MainPageActivity extends AppCompatActivity {
 
     private static final int WRITE_EXTERNAL_STORAGE_CODE = 1;
     private static final String TAG = "aici";
+    private static final String TEXT = "testText";
 
-    private TextView textView;
+
+    private TextView priceText, locationText;
     private SurfaceView surfaceView;
-
     private CameraSource cameraSource;
     private TextRecognizer textRecognizer;
-
     private TextToSpeech textToSpeech;
     private String stringResult = null;
-
     private RadioGroup radioGroup;
-
     private Button mLogout, mSave;
-
     private String recipientList = "tudor.alin97@yahoo.com, tudortudor2802@gmail.com";
+    private String receiptTotal = "nedetectat";
+    private String detectedLocation = "nedetectat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ActivityCompat.requestPermissions(this, new String[]{CAMERA}, PackageManager.PERMISSION_GRANTED);
+
         textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
             }
         });
+
         mLogout = findViewById(R.id.logout);
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,6 +97,7 @@ public class MainPageActivity extends AppCompatActivity {
                 }
             }
         });
+
 //        mSave = findViewById(R.id.save);
 //        mSave.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -106,7 +113,7 @@ public class MainPageActivity extends AppCompatActivity {
 //        cameraSource.release();
 //    }
 
-    private void textRecognizer(){
+    private void textRecognizer() {
         textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
         cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
                 .setRequestedPreviewSize(1280, 1024)
@@ -142,12 +149,41 @@ public class MainPageActivity extends AppCompatActivity {
 
             @Override
             public void receiveDetections(Detector.Detections<TextBlock> detections) {
-
+                //final String regExp = "^[0-9]+([,.][0-9]{1,2})?";
                 SparseArray<TextBlock> sparseArray = detections.getDetectedItems();
                 StringBuilder stringBuilder = new StringBuilder();
 
-                for (int i = 0; i<sparseArray.size(); ++i){
+                // first or second block should indicate the location
+                if ((sparseArray.valueAt(0).getValue().matches("[\\s\\S]*S[.]*C[.]*[\\s\\S]*")) ||
+                        (sparseArray.valueAt(0).getValue().matches("[\\s\\S]*S[.]*R[.]*L[.]*[\\s\\S]*")) ||
+                        (sparseArray.valueAt(0).getValue().matches("[\\s\\S]*S[.]*A[.]*[\\s\\S]*"))) {
+                    detectedLocation = sparseArray.valueAt(0).getValue();
+                } else if ((sparseArray.valueAt(1).getValue().matches("[\\s\\S]*S[.]*C[.]*[\\s\\S]*")) ||
+                        (sparseArray.valueAt(1).getValue().matches("[\\s\\S]*S[.]*R[.]*L[.]*[\\s\\S]*")) ||
+                        (sparseArray.valueAt(1).getValue().matches("[\\s\\S]*S[.]*A[.]*[\\s\\S]*"))) {
+                    detectedLocation = sparseArray.valueAt(1).getValue();
+                }
+
+                for (int i = 0; i<sparseArray.size(); ++i) {
                     TextBlock textBlock = sparseArray.valueAt(i);
+                    //String[] sumKeywords = {"total", "suma", "suma totala", "pret", "cash"};
+//                    Log.v(TEXT, textBlock.getValue().toLowerCase());
+//                    if (textBlock.getValue().toLowerCase().matches("^.*penny*.|.*kaufland*.$")) {
+//                        Log.v(TAG, "detected location: " + detectedLocation);
+//                        detectedLocation = textBlock.getValue().toLowerCase();
+//                    }
+                    if (textBlock.getValue().toLowerCase().matches("^.*total*.|.*suma*.|.*subtotal*.$")) {
+                        if (sparseArray.valueAt(i + 1).getValue().matches("[+-]?(?=\\d*[.eE])(?=\\.?\\d)\\d*\\.?\\d*(?:[eE][+-]?\\d+)?")) {
+                            receiptTotal = sparseArray.valueAt(i + 1).getValue();
+                        }
+                       // final Pattern pattern = Pattern.compile(regExp);
+                       // Matcher matcher = pattern.matcher(receiptTotal);
+//                        if (!matcher.matches()) {
+//                            receiptTotal = "nedetectat";
+//                        }
+                    }
+                    Log.v(TAG, "position " + i + "text: " + textBlock.getValue());
+                    //Log.v(TAG, textBlock.getValue());
                     if (textBlock != null && textBlock.getValue() !=null){
                         stringBuilder.append(textBlock.getValue() + " ");
                     }
@@ -173,33 +209,59 @@ public class MainPageActivity extends AppCompatActivity {
     }
 
     public void takePhoto(View view) {
-        String stringResult1 = "initial";
-        String price = "";
-        setContentView(R.layout.activity_main);
-        //textView = findViewById(R.id.textView);
-        char resultArray[] = stringResult.toCharArray();
-        for (int i = 0; i < stringResult.length(); i++) {
-            if(resultArray[i] == '.' && (resultArray[i-1] >= '0' && resultArray[i-1] <= '9') && (resultArray[i+1] >= '0' && resultArray[i+1] <= '9')) {
-                int j = i;
-                while (resultArray[j-1] >= '0' && resultArray[j-1] <= '9') {
-                    price = price + resultArray[j-1];
-                    j--;
+        setContentView(R.layout.activity_receipt_scan_save);
+
+        priceText = findViewById(R.id.scannedAmount);
+        locationText = findViewById(R.id.detectedShop);
+
+        float maxPrice = 0;
+        float secondMax = 0;
+        //Log.v(TAG, "result: " + stringResult);
+
+        String[] resultArray = stringResult.split("\\s+");
+        for(int i = 0; i < resultArray.length; i++) {
+            //Log.v(TAG, "resultArray["+i+"]: " + resultArray[i]);
+            // detect shop, hardcoded for now
+//            if (resultArray[i].toLowerCase().matches(".*penny.*|.*kaufland*.")) {
+//                detectedLocation = resultArray[i];
+//            }
+
+            // if the price was not detected on the surface view
+            if (receiptTotal.equals("nedetectat")) {
+
+                // replace , with .
+                char[] resultChar = resultArray[i].toCharArray();
+                for (int j = 0; j < resultChar.length; j++) {
+                    if (resultChar[j] == ',') resultChar[j] = '.';
                 }
-                price = new StringBuilder(price).reverse().toString();
-                j = i;
-                price = price + ".";
-                while (resultArray[j+1] >= '0' && resultArray[j+1] <= '9') {
-                    price = price + resultArray[j+1];
-                    j++;
+                resultArray[i] = String.valueOf(resultChar);
+
+                boolean isFloat = resultArray[i].matches("[+-]?(?=\\d*[.eE])(?=\\.?\\d)\\d*\\.?\\d*(?:[eE][+-]?\\d+)?");
+
+                if (isFloat) {
+                    float floatResult = Float.parseFloat(resultArray[i]);
+                    if (floatResult >= maxPrice) {
+                        secondMax = maxPrice;
+                        maxPrice = floatResult;
+                    }
                 }
-                stringResult1 = "OK!";
             }
         }
-        //textView.setText(price);
+        if ((maxPrice > 0) & (receiptTotal.equals("nedetectat"))) {
+            String secondMaxStr = String.format("%.02f", secondMax);
+            receiptTotal = secondMaxStr;
+        }
+        //Intent intent = new Intent(this, ReceiptScanSaveActivity.class);
+        //startActivity(intent);
+
+        priceText.setText(receiptTotal);
+        locationText.setText(detectedLocation);
+
         //textToSpeech.speak(stringResult, TextToSpeech.QUEUE_FLUSH, null, null);
-        Toast.makeText(MainPageActivity.this,
-                "Imaginea a fost scanată cu succes!", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(MainPageActivity.this,
+//                "Bonul a fost scanat cu succes!", Toast.LENGTH_SHORT).show();
     }
+
 
     public void writeToFile(View view) {
         try {
--- app/src/main/java/com/example/conversieimaginetext/receiptScan/ReceiptScanSaveActivity.java
+++ app/src/main/java/com/example/conversieimaginetext/receiptScan/ReceiptScanSaveActivity.java
@@ -3,14 +3,33 @@ package com.example.conversieimaginetext.receiptScan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.conversieimaginetext.R;
+import com.google.android.gms.vision.CameraSource;
+
+import java.io.IOException;
 
 public class ReceiptScanSaveActivity extends AppCompatActivity {
+
+    private static final String TAG = "aici";
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt_scan_save);
+
     }
+
+    public void goBack(View view) {
+        //finish();
+        setContentView(R.layout.activity_main);
+    }
+
 }
